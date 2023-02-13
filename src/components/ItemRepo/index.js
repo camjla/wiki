2131f62685dc021695f9c132c64fab5b9e609a87
Camila Lima <camila.tronco@hotmail.com>
2023-02-13 19:39:52 -0300

    Organizando estilos e pastas

---
+++ src/components/ItemRepo/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { ItemContainer } from './styles'
+
+function ItemRepo() {
+  return (
+    <ItemContainer>
+      <h3>oi</h3>
+      <p>iuiu</p>
+      <hr></hr>
+    </ItemContainer>
+  )
+}
+export default ItemRepo
---
+++ src/pages/App.js
@@ -0,0 +1,16 @@
+import gitLogo from '../assets/github.png'
+import { Container } from './styles'
+import Input from '../components/Input'
+import ItemRepo from '../components/ItemRepo'
+
+function App() {
+  return (
+    <Container>
+      <img src={gitLogo} width={72} height={72} />
+      <Input />
+      <ItemRepo />
+    </Container>
+  )
+}
+
+export default App
